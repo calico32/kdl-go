ba96148eddd489e280e685cc64b24ea9ad598131
Caleb Chan <cchan@hey.com>
2025-12-19 14:09:48 -0500

    feat: add encoder

---
+++ encoder.go
@@ -0,0 +1,496 @@
+package kdl
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"slices"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func Encode(v any, w io.Writer, opts ...EmitterOption) error {
+	doc, err := Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	err = Emit(doc, w, opts...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type MarshalOption func(*encoder)
+
+func Marshal(v any, opts ...MarshalOption) (*Document, error) {
+	var target reflect.Value
+	if rv, ok := v.(reflect.Value); ok {
+		target = rv
+	} else {
+		target = reflect.ValueOf(v)
+		if !target.IsValid() {
+			return nil, errors.Errorf("cannot marshal nil value")
+		}
+		for target.Kind() == reflect.Pointer {
+			target = target.Elem()
+			if !target.IsValid() {
+				return nil, errors.Errorf("cannot marshal nil pointer")
+			}
+		}
+
+		if target.Kind() == reflect.Interface {
+			target = target.Elem()
+			if !target.IsValid() {
+				return nil, errors.Errorf("cannot marshal nil interface")
+			}
+			for target.Kind() == reflect.Pointer {
+				target = target.Elem()
+				if !target.IsValid() {
+					return nil, errors.Errorf("cannot marshal nil pointer inside interface")
+				}
+			}
+		}
+	}
+
+	doc := &Document{}
+	e := &encoder{
+		stack: []*Document{doc},
+	}
+	for _, opt := range opts {
+		opt(e)
+	}
+	defer un(e.trace("Marshal %s", target.Type()))
+	switch target.Kind() {
+	case reflect.Struct:
+		err := e.encodeStructFieldsAsNodes(target)
+		if err != nil {
+			return nil, err
+		}
+	case reflect.Map:
+		err := e.encodeMapEntriesAsNodes(target)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.Errorf("argument must be struct or map (marshaling document, got %s)", target.Kind())
+	}
+
+	return doc, nil
+}
+
+func WithTrace(w io.Writer) MarshalOption {
+	return func(e *encoder) {
+		e.traceWriter = w
+	}
+}
+
+type encoder struct {
+	stack       []*Document // required
+	traceWriter io.Writer
+	indent      int
+}
+
+func (e *encoder) tracef(format string, args ...any) {
+	if e.traceWriter != nil {
+		fmt.Fprintf(e.traceWriter, "%s", strings.Repeat("  ", e.indent))
+		fmt.Fprintf(e.traceWriter, format, args...)
+	}
+}
+
+func (e *encoder) trace(format string, args ...any) func() {
+	e.tracef(format+"\n", args...)
+	e.indent++
+	return func() {
+		e.indent--
+	}
+}
+
+func un(f func()) {
+	f()
+}
+
+func (e *encoder) currentContext() *Document {
+	return e.stack[len(e.stack)-1]
+}
+
+func (e *encoder) pushContext(doc *Document) {
+	e.stack = append(e.stack, doc)
+}
+
+func (e *encoder) popContext() {
+	e.stack = e.stack[:len(e.stack)-1]
+}
+
+func (e *encoder) encodeStructFieldsAsNodes(target reflect.Value) error {
+	defer un(e.trace("encodeStructFieldsAsNodes %s", target.Type()))
+	ctx, err := newStructContext(target.Type())
+	if err != nil {
+		return err
+	}
+
+	for i := range target.NumField() {
+		tag := ctx.tags[i]
+		if tag.name == "" || tag.name == "-" {
+			continue
+		}
+		field := target.Field(i)
+		if !field.CanInterface() {
+			panic(fmt.Sprintf("kdl.encoder: unexported field %s.%s, should be unreachable",
+				target.Type(), target.Type().Field(i).Name))
+		}
+
+		if tag.flags&omitzero != 0 && field.IsZero() {
+			continue
+		}
+
+		err := e.encodeValueAsNode(tag.name, tag, field)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (e *encoder) encodeStructAsNode(name string, target reflect.Value) error {
+	defer un(e.trace("encodeStructAsNode %s", target.Type()))
+	ctx, err := newStructContext(target.Type())
+	if err != nil {
+		return err
+	}
+
+	node := NewNode(name)
+
+	for fieldIndex := range target.Type().NumField() {
+		tag := ctx.tags[fieldIndex]
+		if tag.name == "-" {
+			continue
+		}
+		field := target.Field(fieldIndex)
+		if !field.CanInterface() {
+			panic(fmt.Sprintf("kdl.encoder: unexported field %s.%s, should be unreachable",
+				target.Type(), target.Type().Field(fieldIndex).Name))
+		}
+
+		if tag.flags&omitzero != 0 && field.IsZero() {
+			continue
+		}
+
+		if tag.flags&argument != 0 {
+			e.tracef("argument: %s %s\n", tag.name, field.Type())
+			value, err := TryNewValue(field.Interface())
+			if err != nil {
+				return err
+			}
+			node.AddArgument(value)
+			continue
+		}
+
+		if tag.flags&arguments != 0 {
+			for i := 0; i < field.Len(); i++ {
+				e.tracef("arguments: %s[%d] %s\n", tag.name, i, field.Index(i).Type())
+				if tag.flags&omitzero != 0 && field.Index(i).IsZero() {
+					continue
+				}
+				value, err := TryNewValue(field.Index(i).Interface())
+				if err != nil {
+					return err
+				}
+				node.AddArgument(value)
+			}
+			continue
+		}
+
+		if tag.flags&property != 0 {
+			e.tracef("property: %s %s\n", tag.name, field.Type())
+			value, err := TryNewValue(field.Interface())
+			if err != nil {
+				return err
+			}
+			node.AddProperty(tag.name, value)
+			continue
+		}
+
+		if tag.flags&properties != 0 {
+			e.tracef("properties: %s %s\n", tag.name, field.Type())
+			switch field.Kind() {
+			case reflect.Map:
+				for _, key := range field.MapKeys() {
+					val := field.MapIndex(key)
+					if tag.flags&omitzero != 0 && val.IsZero() {
+						continue
+					}
+					value, err := TryNewValue(val.Interface())
+					if err != nil {
+						return err
+					}
+					node.AddProperty(fmt.Sprint(key.Interface()), value)
+				}
+			case reflect.Struct:
+				err := e.encodeStructIntoProperties(node, field)
+				if err != nil {
+					return err
+				}
+			default:
+				return errors.Errorf("unsupported properties field kind %s", field.Kind())
+			}
+			continue
+		}
+
+		if tag.flags&children != 0 {
+			e.tracef("children: %s %s\n", tag.name, field.Type())
+			switch field.Kind() {
+			case reflect.Map:
+				e.pushContext(node.Children())
+				err := e.encodeMapEntriesAsNodes(field)
+				if err != nil {
+					return err
+				}
+				e.popContext()
+				continue
+			case reflect.Struct:
+				e.pushContext(node.Children())
+				err := e.encodeStructFieldsAsNodes(field)
+				if err != nil {
+					return err
+				}
+				e.popContext()
+				continue
+			default:
+				return errors.Errorf("unsupported children field kind %s", field.Kind())
+			}
+		}
+
+		// no special flags, treat as child node
+		e.pushContext(node.Children())
+		err := e.encodeValueAsNode(tag.name, tag, field)
+		if err != nil {
+			return err
+		}
+		e.popContext()
+	}
+
+	e.currentContext().AddNode(node)
+	return nil
+}
+
+func (e *encoder) encodeMapAsNode(name string, target reflect.Value) error {
+	defer un(e.trace("encodeMapAsNode %s", target.Type()))
+	node := NewNode(name)
+	e.pushContext(node.Children())
+	err := e.encodeMapEntriesAsNodes(target)
+	if err != nil {
+		return err
+	}
+	e.popContext()
+	e.currentContext().AddNode(node)
+	return nil
+}
+
+type mapKey struct {
+	string
+	reflect.Value
+}
+
+func (e *encoder) encodeMapEntriesAsNodes(target reflect.Value) error {
+	defer un(e.trace("encodeMapEntriesAsNodes %s", target.Type()))
+	keys := make([]mapKey, 0, target.Len())
+	for _, key := range target.MapKeys() {
+		keys = append(keys, mapKey{
+			fmt.Sprint(key.Interface()),
+			key,
+		})
+	}
+	slices.SortStableFunc(keys, func(a, b mapKey) int {
+		return strings.Compare(a.string, b.string)
+	})
+
+	for _, key := range keys {
+		val := target.MapIndex(key.Value)
+		err := e.encodeValueAsNode(key.string, structTag{}, val)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *encoder) encodeValueAsNode(name string, tag structTag, target reflect.Value) error {
+	defer un(e.trace("encodeValueAsNode %s %s", name, target.Type()))
+
+	if encoded, err := e.tryEncodeCustomMarshalerAsNode(name, target); encoded || err != nil {
+		return err
+	}
+
+	for target.Kind() == reflect.Pointer {
+		if encoded, err := e.tryEncodeCustomMarshalerAsNode(name, target); encoded || err != nil {
+			return err
+		}
+		target = target.Elem()
+	}
+
+	switch target.Kind() {
+	case reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64, reflect.Bool:
+		value, err := TryNewValue(target.Interface())
+		if err != nil {
+			return err
+		}
+		childNode := NewKValue(name, value)
+		e.currentContext().AddNode(childNode)
+
+	case reflect.Struct:
+		err := e.encodeStructAsNode(name, target)
+		if err != nil {
+			return err
+		}
+
+	case reflect.Slice, reflect.Array:
+		if tag.flags&multiple != 0 {
+			// each element becomes its own node
+			for i := 0; i < target.Len(); i++ {
+				err := e.encodeValueAsNode(name, tag, target.Index(i))
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		child := NewNode(name)
+		for i := 0; i < target.Len(); i++ {
+			value, err := TryNewValue(target.Index(i).Interface())
+			if err != nil {
+				return err
+			}
+			child.AddArgument(value)
+		}
+		e.currentContext().AddNode(child)
+
+	case reflect.Map:
+		err := e.encodeMapAsNode(name, target)
+		if err != nil {
+			return err
+		}
+
+	default:
+		return errors.Errorf("unsupported value kind %s", target.Kind())
+	}
+	return nil
+}
+
+func (e *encoder) encodeStructIntoProperties(node *Node, target reflect.Value) error {
+	defer un(e.trace("encodeStructIntoProperties %s", target.Type()))
+	ctx, err := newStructContext(target.Type())
+	if err != nil {
+		return err
+	}
+
+	for i := range target.NumField() {
+		tag := ctx.tags[i]
+		if tag.name == "" || tag.name == "-" {
+			continue
+		}
+		field := target.Field(i)
+		if !field.CanInterface() {
+			panic(fmt.Sprintf("kdl.encoder: unexported field %s.%s, should be unreachable",
+				target.Type(), target.Type().Field(i).Name))
+		}
+
+		if tag.flags&omitzero != 0 && field.IsZero() {
+			continue
+		}
+
+		value, err := TryNewValue(field.Interface())
+		if err != nil {
+			return err
+		}
+		node.AddProperty(tag.name, value)
+	}
+
+	return nil
+}
+
+func (e *encoder) tryEncodeCustomMarshalerAsNode(name string, target reflect.Value) (bool, error) {
+	defer un(e.trace("tryEncodeCustomMarshalerAsNode %s %s", name, target.Type()))
+	getPointer := func() reflect.Value {
+		if target.CanAddr() {
+			return target.Addr()
+		}
+
+		// make addressable copy
+		copy := reflect.New(target.Type()).Elem()
+		copy.Set(target)
+		return copy.Addr()
+	}
+
+	if target.Type().Implements(reflect.TypeFor[Marshaler]()) {
+		n, err := target.Interface().(Marshaler).MarshalKDL()
+		if err != nil {
+			return false, err
+		}
+		if n.name == "" {
+			n.name = name
+		}
+		e.currentContext().AddNode(n)
+		return true, nil
+	}
+
+	if reflect.PointerTo(target.Type()).Implements(reflect.TypeFor[Marshaler]()) {
+		ptr := getPointer()
+		n, err := ptr.Interface().(Marshaler).MarshalKDL()
+		if err != nil {
+			return false, err
+		}
+		if n.name == "" {
+			n.name = name
+		}
+		e.currentContext().AddNode(n)
+		return true, nil
+	}
+
+	if target.Type().Implements(reflect.TypeFor[DocumentMarshaler]()) {
+		doc, err := target.Interface().(DocumentMarshaler).MarshalKDLDocument()
+		if err != nil {
+			return false, err
+		}
+		e.currentContext().AddNode(NewNode(name).AddChildren(doc.Nodes...))
+		return true, nil
+	}
+
+	if reflect.PointerTo(target.Type()).Implements(reflect.TypeFor[DocumentMarshaler]()) {
+		ptr := getPointer()
+		doc, err := ptr.Interface().(DocumentMarshaler).MarshalKDLDocument()
+		if err != nil {
+			return false, err
+		}
+		e.currentContext().AddNode(NewNode(name).AddChildren(doc.Nodes...))
+		return true, nil
+	}
+
+	if target.Type().Implements(reflect.TypeFor[ValueMarshaler]()) {
+		v, err := target.Interface().(ValueMarshaler).MarshalKDLValue()
+		if err != nil {
+			return false, err
+		}
+		childNode := NewKValue(name, v)
+		e.currentContext().AddNode(childNode)
+		return true, nil
+	}
+
+	if reflect.PointerTo(target.Type()).Implements(reflect.TypeFor[ValueMarshaler]()) {
+		ptr := getPointer()
+		v, err := ptr.Interface().(ValueMarshaler).MarshalKDLValue()
+		if err != nil {
+			return false, err
+		}
+		childNode := NewKValue(name, v)
+		e.currentContext().AddNode(childNode)
+		return true, nil
+	}
+
+	return false, nil
+}
---
+++ encoder_test.go
@@ -0,0 +1,241 @@
+package kdl_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/calico32/kdl-go"
+)
+
+type EncoderPerson struct {
+	Name    string `kdl:"name"`
+	Age     int    `kdl:"age,omitzero"`
+	Married bool   `kdl:"married,omitzero"`
+}
+
+type EncoderBook struct {
+	Title  string        `kdl:"title"`
+	Author EncoderPerson `kdl:"author,omitzero"`
+}
+
+type EncoderPeople struct {
+	People []EncoderPersonProps `kdl:"person,omitzero,multiple"`
+}
+
+type EncoderPersonProps struct {
+	Name    string            `kdl:",arg"`
+	Age     int               `kdl:"age,prop,omitzero"`
+	Married bool              `kdl:"married,prop,omitzero"`
+	Extra   map[string]string `kdl:",props,omitzero"`
+	Args    []string          `kdl:",args,omitzero"`
+}
+
+type EncoderPeopleGroups struct {
+	Groups []EncoderMapStruct `kdl:"group,multiple"`
+}
+
+type EncoderMapStruct struct {
+	Name   string                          `kdl:",arg"`
+	People map[string]EncoderPersonDetails `kdl:",children"`
+}
+
+type EncoderPersonDetails struct {
+	Age     int  `kdl:"age,prop"`
+	Married bool `kdl:"married,prop"`
+}
+
+type EncoderCustomNodeMarshaler struct {
+	Foo string
+	Bar int
+}
+
+var _ kdl.Marshaler = &EncoderCustomNodeMarshaler{}
+
+func (e *EncoderCustomNodeMarshaler) MarshalKDL() (*kdl.Node, error) {
+	node := kdl.NewNode("custom")
+	node.AddArgument(kdl.NewString(e.Foo + "-custom"))
+	node.AddProperty("bar", kdl.NewInt(e.Bar*10))
+	return node, nil
+}
+
+type EncoderCustomValueMarshaler string
+
+var _ kdl.ValueMarshaler = EncoderCustomValueMarshaler("")
+
+func (e EncoderCustomValueMarshaler) MarshalKDLValue() (kdl.Value, error) {
+	return kdl.NewString("value-" + string(e)), nil
+}
+
+type EncoderCustomDocumentMarshaler struct {
+	Title string
+	Count int
+}
+
+var _ kdl.DocumentMarshaler = &EncoderCustomDocumentMarshaler{}
+
+func (e *EncoderCustomDocumentMarshaler) MarshalKDLDocument() (*kdl.Document, error) {
+	doc := &kdl.Document{}
+	doc.Nodes = append(doc.Nodes, kdl.NewKV("custom-title", e.Title))
+	doc.Nodes = append(doc.Nodes, kdl.NewKV("custom-count", e.Count))
+	return doc, nil
+}
+
+var encoderTests = []struct {
+	name     string
+	value    any
+	expected string
+}{
+	{"basic",
+		EncoderPerson{
+			Name:    "Alice",
+			Age:     30,
+			Married: true,
+		},
+		`
+			name Alice
+			age 30
+			married #true
+		`,
+	},
+	{"omitzero",
+		EncoderPerson{
+			Name: "Bob",
+		},
+		`
+			name Bob
+		`,
+	},
+	{"nested struct",
+		EncoderBook{
+			Title: "Go Programming",
+			Author: EncoderPerson{
+				Name:    "Carol",
+				Age:     28,
+				Married: true,
+			},
+		},
+		`
+			title "Go Programming"
+			author {
+				name Carol
+				age 28
+				married #true
+			}
+		`,
+	},
+	{"flags",
+		EncoderPeople{
+			People: []EncoderPersonProps{
+				{Name: "Dave", Age: 40, Extra: map[string]string{"job": "Engineer", "hobby": "golf", "foo": ""}},
+				{Name: "Eve", Married: true, Args: []string{"extra1", "", "extra2"}},
+			},
+		},
+		`
+			person Dave age=40 job=Engineer hobby=golf
+			person Eve married=#true extra1 extra2
+		`,
+	},
+	{"map of structs",
+		EncoderPeopleGroups{
+			Groups: []EncoderMapStruct{
+				{
+					Name: "Group1",
+					People: map[string]EncoderPersonDetails{
+						"Frank": {Age: 35, Married: false},
+						"Grace": {Age: 29, Married: true},
+					},
+				},
+				{
+					Name: "Group2",
+					People: map[string]EncoderPersonDetails{
+						"Heidi": {Age: 32, Married: true},
+					},
+				},
+			},
+		},
+		`
+			group Group1 {
+				Frank age=35 married=#false
+				Grace age=29 married=#true
+			}
+			group Group2 {
+				Heidi age=32 married=#true
+			}
+		`,
+	},
+	{
+		"custom marshalers",
+		struct {
+			Node  EncoderCustomNodeMarshaler     `kdl:"node"`
+			Value EncoderCustomValueMarshaler    `kdl:"value"`
+			Doc   EncoderCustomDocumentMarshaler `kdl:"doc"`
+		}{
+			Node: EncoderCustomNodeMarshaler{
+				Foo: "test",
+				Bar: 7,
+			},
+			Value: EncoderCustomValueMarshaler("qux"),
+			Doc: EncoderCustomDocumentMarshaler{
+				Title: "My Document",
+				Count: 3,
+			},
+		},
+		`
+			custom test-custom bar=70
+			value value-qux
+			doc {
+				custom-title "My Document"
+				custom-count 3
+			}
+		`,
+	},
+}
+
+func TestEncoder(t *testing.T) {
+	for _, tt := range encoderTests {
+		t.Run(tt.name, func(t *testing.T) {
+			doc, err := kdl.Marshal(tt.value)
+			if err != nil {
+				t.Errorf("unexpected error during marshaling: %+v", err)
+				return
+			}
+			actualStr := new(bytes.Buffer)
+			err = kdl.Emit(doc, actualStr)
+			if err != nil {
+				t.Errorf("unexpected error during emitting: %+v", err)
+				return
+			}
+
+			actual, err := roundtrip(actualStr.String())
+			if err != nil {
+				t.Errorf("unexpected error during actual roundtrip: %+v", err)
+				return
+			}
+
+			expected, err := roundtrip(tt.expected)
+			if err != nil {
+				t.Errorf("unexpected error during expected roundtrip: %+v", err)
+				return
+			}
+
+			if actual != expected {
+				t.Errorf("marshaled document does not match expected\nExpected:\n%s\nGot:\n%s", expected, actual)
+			}
+		})
+	}
+}
+
+func roundtrip(doc string) (string, error) {
+	parsed, err := kdl.Parse(strings.NewReader(doc))
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	err = kdl.Emit(parsed, buf)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
